0ebff892f40a12726da0399cac11f095e0d23f42
Usuario <Usuario@DESKTOP-08J8VIT>
2024-10-26 22:44:18 -0500

    zzzz

--- InventarioApp/src/main/java/conexion/BD.java
+++ InventarioApp/src/main/java/conexion/BD.java
@@ -4,10 +4,34 @@
  */
 package conexion;
 
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Usuario
  */
 public class BD {
     
+    Connection conectar = null;
+    
+    String usuario = "lestad";
+    String contraseña = "Cr7goat2023";
+    String bd="RADIOSHACK1";
+    String ip="localhost";
+    String puerto="1433";
+    
+    String cadena="jdbc:sqlserver://"+ip+":"+puerto+"/"+bd;
+    
+    public Connection establecerConexion(){
+    
+    try{
+        String cadena="jbd:sqlserver://localhost:"+puerto+":"+"databaseName"+bd;
+        conectar=DriverManager.getConnection(cadena,usuario,contraseña);
+        JOptionPane.showMessageDialog(null, "Se conecto correctamente a la Base de Datos");
+    } catch (Exception e){
+        JOptionPane.showMessageDialog(null,"Error al conectarse a la base de datos, error:" + e.toString());
+    }
+    return conectar;
+    }
 }
